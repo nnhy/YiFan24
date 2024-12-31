b16b1c091287612ae47a1a9a869da9968b4bb74d
大石头 <nnhy@vip.qq.com>
2024-12-31 20:31:33 +0800

    添加项目文件。

---
+++ 快算24点/Program.cs
@@ -0,0 +1,28 @@
+﻿using NewLife;
+using NewLife.Data;
+using NewLife.Log;
+
+XTrace.UseConsole();
+
+Console.WriteLine("一凡快算24点，输入逗号隔开的4个数字。");
+while (true)
+{
+    Console.Write("请输入4个数字：");
+    var line = Console.ReadLine();
+
+    //var nums = new Double[] { 1, 5, 5, 5 };
+    var nums = line.SplitAsInt().Select(e => (Double)e).ToArray();
+    XTrace.WriteLine("开始24点运算：{0}", nums);
+
+    var bt = new BinaryTree();
+    //bt.Operations.Add("Sqrt");
+    //bt.Operations.Add("Cbrt");
+    var ss = bt.Execute(nums, 24);
+    XTrace.WriteLine("共有结果：{0}", ss.Length);
+    foreach (var item in ss)
+    {
+        XTrace.WriteLine(item);
+    }
+}
+
+Console.ReadKey();
